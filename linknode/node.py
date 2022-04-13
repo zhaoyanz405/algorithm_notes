9c50d5c263e2c512900cad9b94e284b9b7c298ef
zhaoyanz405 <zhaoyanz405@gmail.com>
2022-04-13 15:15:50 +0800

    feat: could create linknode by list

--- linknode/node.py
+++ linknode/node.py
@@ -15,3 +15,26 @@ class LinkNode:
             node = node.next
 
         print('->'.join([str(v) for v in vals]))
+
+
+def create_linknode(l):
+    if not l:
+        return None
+
+    head = None
+    cur = None
+
+    for v in l:
+        node = LinkNode(v)
+        if head is None:
+            cur = head = node
+        else:
+            cur.next = node
+            cur = cur.next
+
+    return head
+
+
+if __name__ == '__main__':
+    n = create_linknode([1, 2, 3, 4, 5])
+    n.print()
